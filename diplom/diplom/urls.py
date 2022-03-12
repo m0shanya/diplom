f0a320ee4597e1f3ef14a65dfe2e6d875b07f77d
m0shanya <funnytimka@gmail.com>
2022-03-13 00:00:55 +0300

    some changes in details.html and media

--- diplom/diplom/urls.py
+++ diplom/diplom/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 from cart.views import cart_detail, cart_add, cart_remove
 from shop.views import prod_list, user_login, logout_view, product_details_view
 
@@ -30,4 +33,4 @@ urlpatterns = [
     path('remove/<int:product_id>', cart_remove, name='cart_remove'),
     path('login/', user_login, name='login'),
     path('logout/', logout_view, name='logout'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
