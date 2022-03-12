da37a8e611cabb26bd48c87bacf3c72dbbf3d4f4
m0shanya <funnytimka@gmail.com>
2022-03-12 22:29:33 +0300

    add navbar, validation, cart

--- diplom/shop/views.py
+++ diplom/shop/views.py
@@ -20,34 +20,39 @@ def product_details_view(request, product_id):
     product = get_object_or_404(Product, id=product_id)
     product_cart_form = CartAddProductForm()
     if request.method == "POST":
-        if request.POST.get("count"):
-            Buy.objects.create(purchase=product, user=request.user, count=request.POST.get("count"))
-            return redirect("product_details_view", product_id=product_id)
+        if request.user.is_authenticated:
+            if request.POST.get("count"):
+                Buy.objects.create(purchase=product, user=request.user, count=request.POST.get("count"))
+                return redirect("product_details_view", product_id=product_id)
+        else:
+            return redirect('/login/', )
     return render(request, "shop/details.html", {"product": product, 'product_cart_form': product_cart_form})
 
 
 def user_login(request):
-    if request.method == 'POST':
-        form = LoginForm(request.POST)
-        if form.is_valid():
-            cd = form.cleaned_data
-            user = authenticate(username=cd['username'], password=cd['password'])
-            if user is not None:
-                if user.is_active:
-                    login(request, user)
-                    logger.info(request, f"Authenticated successfully")
-                    return redirect('/', )
-                else:
-                    return HttpResponse('Disabled account')
-            else:
-                return HttpResponse('Invalid login')
+    if request.user.is_authenticated:
+        return render(request, 'shop/login.html', )
     else:
-        form = LoginForm()
-    return render(request, 'shop/login.html', {'form': form})
+        if request.method == 'POST':
+            form = LoginForm(request.POST)
+            if form.is_valid():
+                cd = form.cleaned_data
+                user = authenticate(username=cd['username'], password=cd['password'])
+                if user is not None:
+                    if user.is_active:
+                        login(request, user)
+                        logger.info(request, f"Authenticated successfully")
+                        return redirect('/', )
+                    else:
+                        return HttpResponse('Disabled account')
+                else:
+                    return HttpResponse('Invalid login')
+        else:
+            form = LoginForm()
+        return render(request, 'shop/login.html', {'form': form})
 
 
 def logout_view(request):
-    if request.method == "POST":
+    if request.method == "GET":
         logout(request)
-        return redirect('/', )
-    return render(request, "shop/logout.html", )
+        return render(request, "shop/logout.html", )
